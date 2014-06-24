5085989e0fee4596dafd1d166c7051af429857da
Phisten <ans10528@gmail.com>
2014-06-24 14:10:45 +0800

    init

---
+++ WindowsHidden.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace Phisten.WindowsHidden
+{
+
+
+    class WindowsHidden
+    {
+        static void Main(string[] args)
+        {
+            Process p;
+            //autorun File
+            for (int i = 0,length = args.Length; i < length; i++)
+            {
+                string appPath = args[i];
+                p = openApp(appPath);
+            }
+        }
+
+        private static Process openApp(string appPath)
+        {
+            Process app = new Process();
+            app.StartInfo.FileName = appPath;
+            app.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            app.Start();
+            return app;
+        }
+
+    }
+}
